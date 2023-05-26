4451a84f9025ee51b1294fc08dbedc8ff5788d05
Charlie C <snailrose@gmail.com>
2023-05-26 15:53:29 -0600

    Update

--- View/StringListView.cpp
+++ View/StringListView.cpp
@@ -1,7 +1,28 @@
+/*
+-------------------------------------------------------------------------------
+    Copyright (c) Charles Carley.
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+-------------------------------------------------------------------------------
+*/
 #include "View/StringListView.h"
-#include <QListView>
 #include <QBoxLayout>
-#include <QStringListModel>
+#include <QListView>
+#include <QStandardItemModel>
 #include "View/Colors.h"
 #include "View/Metrics.h"
 #include "View/Qu.h"
@@ -13,16 +34,17 @@ namespace Rt2::View
     StringListView::StringListView(QWidget* parent) :
         View(parent)
     {
-        _listing = new QListView(this);
         construct();
     }
 
     void StringListView::construct()
     {
+        _listing = new QListView(this);
+        _listing->setModel(new QStandardItemModel());
         constructView(_listing);
+
         setBorderColor(Colors::BorderLight);
         setColor(QPalette::AlternateBase, Colors::CtrlBackgroundLight);
-        //setColor(QPalette::AlternateBase, Colors::CtrlBackground);
         setPadding(Metrics::borderSizeThin);
 
         _listing->setSelectionRectVisible(false);
@@ -30,55 +52,59 @@ namespace Rt2::View
         _listing->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
         _listing->setFrameShape(QFrame::NoFrame);
-        bind();
-    }
 
-    void StringListView::itemDoubleClickedImpl(const QModelIndex& index)
-    {
-        const QString str = index.data().toString();
-
-        emit itemDoubleClicked(index.row(), str.toStdString());
-    }
-
-    void StringListView::setList(const StringArray& strList) const
-    {
-        if (!_listing->model())
-            _listing->setModel(new QStringListModel());
-
-        QStringListModel* model = (QStringListModel*)_listing->model();
-
-        QStringList  old = model->stringList();
-        const size_t os  = old.size();
-
-        if (const size_t ns = strList.size();
-            os >= ns)
-        {
-            old.resize(ns);
-            for (size_t i = 0; i < ns; ++i)
-                old[i] = Qsu::to(strList[i]);
-        }
-        else
-        {
-            size_t i = 0;
-            for (; i < os; ++i)
-                old[i] = Qsu::to(strList[i]);
-            for (; i < ns; ++i)
-                old.push_back(Qsu::to(strList[i]));
-        }
-
-        model->setStringList(old);
-    }
-
-    void StringListView::addInput(const ObserverType& ot)
-    {
-        _viewModel.addInput(ot);
-    }
-
-    void StringListView::bind()
-    {
         connect(_listing,
                 &QListView::doubleClicked,
                 this,
                 &StringListView::itemDoubleClickedImpl);
+    }
+
+    void StringListView::itemDoubleClickedImpl(const QModelIndex& index)
+    {
+        _string.setValue(index.data(Qt::UserRole).toString().toStdString(), ViewModel::OUTPUT);
+    }
+
+    void StringListView::setList(const StringArray& strList)
+    {
+        if (!_listing->model())
+            _listing->setModel(new QStandardItemModel());
+        clear();
+        for(const auto &item : strList)
+            addEntry(item);
+    }
+
+    void StringListView::addEntry(const String& string, const String& data) const
+    {
+        if (string.empty()) return;
+
+        if (QStandardItemModel* model = (QStandardItemModel*)_listing->model())
+        {
+            const int cur = model->rowCount();
+            model->insertRows(cur, 1);
+            model->setItem(cur, new QStandardItem(Qsu::to(string)));
+
+            if (!data.empty())
+            {
+                const QModelIndex idx = model->index(cur, 0);
+                model->setData(idx, QVariant(Qsu::to(data)), Qt::UserRole);
+            }
+        }
+    }
+
+    void StringListView::clear()
+    {
+        if (QStandardItemModel* model = (QStandardItemModel*)_listing->model())
+            model->clear();
+        refresh();
+    }
+
+    void StringListView::addInput(const StringListModel::Observer& ot)
+    {
+        _strings.addInput(ot);
+    }
+
+    void StringListView::addOutput(const StringModel::Observer& ot)
+    {
+        _string.addOutput(ot);
     }
 }  // namespace Rt2::View
--- View/StringListView.h
+++ View/StringListView.h
@@ -1,39 +1,58 @@
+/*
+-------------------------------------------------------------------------------
+    Copyright (c) Charles Carley.
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+-------------------------------------------------------------------------------
+*/
 #pragma once
 #include <QListView>
-#include "View/View.h"
 #include "View/Definitions.h"
+#include "View/View.h"
 
 class QLineEdit;
 
 namespace Rt2::View
 {
-
     class StringListView final : public View
     {
         Q_OBJECT
-    public:
-        using ObserverType = StringListModel::Observer;
-
-    signals:
-        void itemDoubleClicked(const int idx, const String& data);
-
     private:
         QListView*      _listing{nullptr};
-        StringListModel _viewModel;
+        StringListModel _strings;
+        StringModel     _string;
 
     public:
         explicit StringListView(QWidget* parent = nullptr);
 
-        void setList(const StringArray& strList) const;
+        [[deprecated]] void setList(const StringArray& strList);
 
-        void addInput(const ObserverType& ot);
+        void addEntry(const String& string, const String& data = "") const;
+
+        void clear();
+
+        void addInput(const StringListModel::Observer& ot);
+
+        void addOutput(const StringModel::Observer& ot);
 
     private:
-        void bind();
-
         void construct();
 
         void itemDoubleClickedImpl(const QModelIndex& index);
     };
 
-}  // namespace Rt2::Widgets
+}  // namespace Rt2::View
